c363418229b6d8afef48ce069a9ddd5e839ee791
azu <azuciao@gmail.com>
2023-02-15 12:18:50 +0900

    fix

--- index.mjs
+++ index.mjs
@@ -1,10 +1,15 @@
 import * as fs from 'fs/promises';
+import * as path from 'path';
 import * as zlib from 'zlib';
 
-const buffer = await fs.readFile('base.dat.gz');
-zlib.gunzip(buffer, (err, buffer) => {
-    if (err) {
-        throw err;
-    }
-    console.log("Success gunzip", buffer);
-});
+const dictFiles = await fs.readdir('dict');
+for (const dictFile of dictFiles) {
+    const fileBuffer = await fs.readFile(path.join('dict', dictFile));
+    zlib.gunzip(fileBuffer, (err, buffer) => {
+        if (err) {
+            console.error("Error:", dictFile, err)
+        } else {
+            console.log("Success:", dictFile);
+        }
+    });
+}
